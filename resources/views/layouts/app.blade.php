342ccec3c2f78508c0fe8a8a586b15e0f1e8ced8
cloco-jagat <jagatjunglakandribk@gmail.com>
2025-04-04 16:50:11 +0545

    tailwind data

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -15,14 +16,36 @@
     <!-- Styles -->
     @vite('resources/css/app.css')
 </head>
+
 <body>
-    <header>
+    {{-- <header>
         <nav>
             <ul>
                 <li><a href="{{ route('home') }}">Home</a></li>
             </ul>
         </nav>
+    </header> --}}
+
+    <header class=" bg-[#003a8a] sticky top-0 py-2 flex  text-white flex-col ">
+        <div class="mb-1 flex justify-center w-full">
+            <div class="w-[1001px] flex justify-end">
+                <button class=" btn bg-slate-950 rounded-2xl">X</button>
+                <button class=" btn bg-slate-950 rounded-2xl">Xasdasd</button>
+                <button class=" btn bg-slate-950 rounded-2xl">asdasdX</button>
+            </div>
+        </div>
+        <div class=" border-b-1 border-blue-500 border-dashed">
+
+        </div>
     </header>
+
+    <div class=" bg-[#f3f0ea] flex justify-center">
+        <div class=" w-[1001px] flex justify-between pt-2 pb-4 ">
+            <div> logo</div>
+            <div> logo</div>
+        </div>
+
+    </div>
 
     <main>
         @yield('content')
@@ -32,4 +55,5 @@
     @vite('resources/js/app.js')
     @yield('footer_scripts')
 </body>
+
 </html>
--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,20 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import laravel from "laravel-vite-plugin";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
-                'resources/js/components/HomeComponent.jsx',
+                "resources/css/app.css",
+                "resources/js/app.js",
+                "resources/js/components/HomeComponent.jsx",
                 // 'resources/js/pages/about.js',
             ],
             refresh: true,
         }),
+        tailwindcss(),
         react(),
     ],
 });
